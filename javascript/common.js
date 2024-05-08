d465c9b15adb5577fc0fa2ab2131042a314c3559
theeasylearn <theeasylearn@gmail.com>
2024-05-08 19:26:43 +0530

    FUNCTIONS

--- javascript/common.js
+++ javascript/common.js
@@ -1,13 +1,16 @@
-let $ = function(id)
-    {
-        let temp = document.getElementById(id);
-        return temp;
-    }
-    function log(...messages)
-    {
-      console.log(messages);
-    }
-    function val(id)
-    {
-        return $(id).value
-    }
+// let $ = function(id)
+// {
+//     let temp = document.getElementById(id);
+//     return temp;
+// }
+let $ = (id) => document.getElementById(id);
+// function log(...messages)
+// {
+//     console.log(messages);
+// }
+let log = (...messages) => console.log(messages);
+// function val(id)
+// {
+//     return $(id).value
+// }
+let val = (id) => $(id).value;
