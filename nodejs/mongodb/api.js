d2f2c2a1b43ef35814a0e3449fa8b3b34e91c810
theeasylearn <theeasylearn@gmail.com>
2024-06-27 10:33:15 +0530

    api

--- nodejs/mongodb/api.js
+++ nodejs/mongodb/api.js
@@ -22,11 +22,43 @@ let insertObject = function (request, response) {
         response.json([{ 'error': 'yes', 'message': 'error in connection' }]);
     });
 }
+//required input
+//city=bhavnagar&newcity=baroda&newpincode=364002
 let updateObject = function (request, response) {
-    response.send('request received for put')
+    if(request.body.city === undefined || request.body.newcity === undefined || request.body.newpincode === undefined)
+    {
+        response.json([{ 'error': 'input is missing'}]);
+
+    }
+    else 
+    {
+        dbPromise.then((database) => {
+            let condition = { 'city': request.body.city };
+            let updatedDocument = { $set: { city: request.body.newcity, pincode: request.body.newpincode } };
+            database.collection('myobjects').updateMany(condition, updatedDocument, function (error, result) {
+                response.json([{ 'error': 'no', 'message': 'object updated' }]);
+            });
+        }).catch((error) => {
+            response.json([{ 'error': 'yes', 'message': 'error in connection' }]);
+        })
+    }
 }
+
+//required input
+//team=afghanistan
 let deleteObject = function (request, response) {
-    response.send('request received for delete')
+    dbPromise.then((database) => {
+
+        var condition = {'team':request.body.team};
+        database.collection('myobjects').deleteMany(condition,function(error,result){
+           if(error)
+               response.json([{ 'error': 'error in delete'}]);
+           else 
+               response.json([{ 'error': 'no', 'message': 'object deleted' }]);
+        })
+    }).catch((error) => {
+        response.json([{ 'error': 'yes', 'message': 'error in connection' }]);
+    })
 }
 app.get(ROUTE,(request,response) => getObject(request,response));
 app.post(ROUTE,(request,response) => insertObject(request,response));
