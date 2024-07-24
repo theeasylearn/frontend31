65546c958a8a644312286a9a2e0411ef1a666990
theeasylearn <theeasylearn@gmail.com>
2024-07-24 10:35:56 +0530

    api

--- nodejs/mysql/users.js
+++ nodejs/mysql/users.js
@@ -3,32 +3,62 @@ const getMissingFields = (body, requiredFields) => {
     return requiredFields.filter(field => !body[field]);
 };
 
-let insert = function(request,response){
-    let { name, email, password, mobileno, city, address, bloodgroup, dob, gender } = request.body; //object destructring 
-  
-    let sql = `insert into register_user (name,email,password,mobileno,city,address,bloodgroup,dob,gender) values ('${name}','${email}','${password}','${mobileno}','${city}','${address}','${bloodgroup}','${dob}','${gender}')`;
+let insert = function (request, response) {
+    let { name, email, password, mobileno, city, address, bloodgroup, dob, gender } = request.body;
 
-    connection.db.query(sql,function(error,result){
-        if(error!=null)
-        {
-            if (error.code === 'ER_DUP_ENTRY')
-            {
-                response.json("[{'error':'no'},{'success':'no'},{'message':'email/mobile is already regisered with us'}]");
-            }    
-            else 
-            {
-                response.json("[{'error':'no'},{'success':'no'},{'message':'something went wrong, please try after someimes'}]");
+    const requiredFields = ['name', 'email', 'password', 'mobileno', 'city', 'address', 'bloodgroup', 'dob', 'gender'];
+    const missingFields = getMissingFields(request.body, requiredFields);
+
+    if (missingFields.length > 0) {
+        response.json({
+            error: 'no',
+            success: 'no',
+            message: `Missing fields: ${missingFields.join(',')}`
+        });
+        return;
+    }
+    else {
+        let sql = `insert into register_user (name,email,password,mobileno,city,address,bloodgroup,dob,gender) values ('${name}','${email}','${password}','${mobileno}','${city}','${address}','${bloodgroup}','${dob}','${gender}')`;
+
+        connection.db.query(sql, function (error, result) {
+            if (error != null) {
+                if (error.code === 'ER_DUP_ENTRY') {
+                    response.json("[{'error':'no'},{'success':'no'},{'message':'email/mobile is already regisered with us'}]");
+                }
+                else {
+                    response.json("[{'error':'no'},{'success':'no'},{'message':'something went wrong, please try after someimes'}]");
+                }
+                console.log(error);
             }
-            console.log(error);
-        }    
-        else 
-        {
-            response.json("[{'error':'no'},{'success':'yes'},{'message':'registered successfully'}]");
-        }
-    });    
+            else {
+                response.json("[{'error':'no'},{'success':'yes'},{'message':'registered successfully'}]");
+            }
+        });
+    }
 }
-let update = function(request,response) {
-    response.send('put request received')
+//change password
+let update = function (request, response) {
+    let requiredFields = ['id', 'oldpassword', 'newpassword'];
+    let missingFields = getMissingFields(request.body, requiredFields);
+    if (missingFields.length > 0) {
+        response.json({
+            error: 'no',
+            success: 'no',
+            message: `Missing fields: ${missingFields.join(',')}`
+        });
+        return;
+    }
+    else {
+        let { id, oldpassword, newpassword } = request.body;
+        let sql = `update register_user set password='${newpassword}' where id=${id}`;
+        connection.db.query(sql, function (error, result) {
+            if (error != null)
+                response.json("[{ 'error': 'error occured' }]");
+            else
+                response.json("[{'error':'no'},{'success':'yes'},{'message':'password changed successfully'}]");
+        });
+    }
+
 }
 module.exports.insert = insert;
 module.exports.update = update;
