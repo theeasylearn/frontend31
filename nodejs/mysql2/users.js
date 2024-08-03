c47fd6ec1e012e609435604317e5f07b796ecb70
theeasylearn <theeasylearn@gmail.com>
2024-08-03 14:30:47 +0530

    login

--- nodejs/mysql2/users.js
+++ nodejs/mysql2/users.js
@@ -15,9 +15,13 @@ let register = function (request, response) {
        let sql = `insert into users (email,password,mobile) values ('${email}','${password}','${mobile}')`;
 
        connection.db.query(sql, function (error, result) {
-           if (error != null) {
-               response.json([{ 'error': "error occured" }]);
+           if (error != null) 
+            {
                console.log(error);
+               if (error.code === 'ER_DUP_ENTRY') 
+                    response.json([{ 'error': "no" },{'success':'no'},{'message':'email/mobile is already registered with us'}]);
+               else 
+                    response.json([{ 'error': "error occured" }]);
            }
            else {
                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'registered successfully' }]);
@@ -27,7 +31,19 @@ let register = function (request, response) {
   
 }
 let login = function (request, response) {
-    response.send("login request received via post method");
+    let sql = "select id from users where email='krish@gmail.com' and password='123123'";
+    connection.db.query(sql,function(error,result){
+        if(error!=null)
+            response.json([{ 'error': "error occured" }]);
+        else 
+        {
+            if(result.length === 0)
+                response.json([{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'invalid login' }]);
+            else 
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'login successfully' }]);
+
+        }
+    });
 }
 let change_password = function (request, response) {
     response.send("change password request received via post method");
