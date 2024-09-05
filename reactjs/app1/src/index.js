ffca7c4d713d39033681c0e0b3a2a6b507b3ba13
theeasylearn <theeasylearn@gmail.com>
2024-09-05 13:44:30 +0530

    bootstrap

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 //create virtual DOM using root from index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-var output = (<div className='container'>
+var output = (
+    <div className='container'>
+    /* start of container */
     <div className='row'>
         <div className='col-12'>
             <h1>Bootstrap in Reactjs</h1>
         </div>
-    </div>
+        <div className='col-12'></div>
+        {/* this is comments */}
+    </div> 
+    /* end of container */
 </div>)
 root.render(output);
