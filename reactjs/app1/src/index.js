4459d01160dc53b061fa8fe6909d885aab13bd66
theeasylearn <theeasylearn@gmail.com>
2024-09-11 15:34:58 +0530

    players

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -3,36 +3,55 @@ import ReactDOM from 'react-dom/client';
 
 //create virtual DOM using root from index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function Product()
-{
-    return (  <div className="col-lg-3">
+function Product(props) {
+    return (<div className="col-lg-3">
         <div className="card shadow">
-            <img className="card-img-top" src="http://picsum.photos/300" />
+            <img className="card-img-top" src={props.photo} />
             <div className="card-body">
-                <h4>IPhone -  16</h4>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus accusantium vero voluptas, totam magnam sit tenetur dolorem maxime aut magni consequuntur ut ipsum non quam quo! Tenetur veniam adipisci, amet, laborum ducimus</p>
+                <h4>{props.name}</h4>
+                <p>{props.detail}</p>
             </div>
             <div className="card-footer">
-                <button className="btn w-100 btn-primary">165000</button>
+                <button className="btn w-100 btn-primary">{props.price}</button>
             </div>
         </div>
     </div>)
 }
-function Shop()
-{
+function Shop() {
     return (
         <div className="container">
-        <div className="row">
-            <div className="col-12">
-                <h1>Products</h1>
-            </div>
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            <div className="row">
+                <div className="col-12">
+                    <h1>Products</h1>
+                </div>
+                <Product
+                    name='iPhone 14 Pro Max'
+                    price='$1099'
+                    photo='https://picsum.photos/200/300?random=1' 
+                    detail='The iPhone 14 Pro Max features a 6.7-inch display, A16 Bionic chip, triple-camera system, and Dynamic Island.'
+                />
+                <Product
+                    name='MacBook Pro 16-inch'
+                    price='$2499'
+                    photo='https://picsum.photos/200/300?random=2' 
+                    detail='The MacBook Pro 16-inch comes with M1 Pro/Max chip, 16-inch Liquid Retina XDR display, and up to 64GB unified memory.'
+                />
+                <Product
+                    name='Apple Watch Series 8'
+                    price='$399'
+                    photo='https://picsum.photos/200/300?random=3' 
+                    detail='Apple Watch Series 8 features advanced health tracking, 50m water resistance, always-on display, and up to 18 hours of battery life.'
+                />
+                <Product
+                    name='AirPods Pro (2nd Generation)'
+                    price='$249'
+                    photo='https://picsum.photos/200/300?random=4' 
+                    detail='The new AirPods Pro come with active noise cancellation, transparency mode, spatial audio, and a MagSafe charging case.'
+                />
 
+
+            </div>
         </div>
-    </div>
     )
 }
 root.render(<Shop />);
