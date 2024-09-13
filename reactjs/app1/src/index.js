c975cd9ec0081af20b91b8e9a535957a00aba608
theeasylearn <theeasylearn@gmail.com>
2024-09-13 15:56:51 +0530

    players

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -3,55 +3,62 @@ import ReactDOM from 'react-dom/client';
 
 //create virtual DOM using root from index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function Product(props) {
-    return (<div className="col-lg-3">
-        <div className="card shadow">
-            <img className="card-img-top" src={props.photo} />
-            <div className="card-body">
-                <h4>{props.name}</h4>
-                <p>{props.detail}</p>
-            </div>
-            <div className="card-footer">
-                <button className="btn w-100 btn-primary">{props.price}</button>
-            </div>
-        </div>
-    </div>)
-}
-function Shop() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <h1>Products</h1>
+//create class components
+class Player extends React.Component
+{
+    constructor(props)
+    {
+        super(props); //calling parent class constuctor (required)
+        //create property (instance variable)
+        this.name = props.name;
+        this.runs = props.runs;
+        this.match = props.match;
+        this.century = props.century;
+        this.fifties = props.fifties;
+        this.average = this.run / this.match;
+        this.photo = props.photo;
+    }
+    //must override below method
+    render()
+    {
+        return (  <div className="col-lg-6">
+            <div className="card shadow">
+              <div className="row">
+                <div className="col-8">
+                  <div className="card-body">
+                    <h3 className="card-title">{this.name}</h3>   
+                    <hr />
+                    Runs : {this.runs} <br />
+                    Match : {this.match} <br />
+                    100s: {this.century} <br />
+                    50s: {this.fifties} <br />
+                  </div>
                 </div>
-                <Product
-                    name='iPhone 14 Pro Max'
-                    price='$1099'
-                    photo='https://picsum.photos/200/300?random=1' 
-                    detail='The iPhone 14 Pro Max features a 6.7-inch display, A16 Bionic chip, triple-camera system, and Dynamic Island.'
-                />
-                <Product
-                    name='MacBook Pro 16-inch'
-                    price='$2499'
-                    photo='https://picsum.photos/200/300?random=2' 
-                    detail='The MacBook Pro 16-inch comes with M1 Pro/Max chip, 16-inch Liquid Retina XDR display, and up to 64GB unified memory.'
-                />
-                <Product
-                    name='Apple Watch Series 8'
-                    price='$399'
-                    photo='https://picsum.photos/200/300?random=3' 
-                    detail='Apple Watch Series 8 features advanced health tracking, 50m water resistance, always-on display, and up to 18 hours of battery life.'
-                />
-                <Product
-                    name='AirPods Pro (2nd Generation)'
-                    price='$249'
-                    photo='https://picsum.photos/200/300?random=4' 
-                    detail='The new AirPods Pro come with active noise cancellation, transparency mode, spatial audio, and a MagSafe charging case.'
-                />
-
-
+                <div className="col-4">
+                  <img src={this.photo} className="img-fluid" />
+                </div>
+              </div>
             </div>
-        </div>
-    )
+          </div>);
+    }
 }
-root.render(<Shop />);
+class Cricket extends React.Component
+{
+    // must Override below method 
+    render()
+    {
+        return (<div className="container">
+            <div className="row">
+              <div className="col-12">
+                <h1>Players</h1>
+              </div>
+              {/* it will automatically call render function of Player class  */}
+             <Player name='KL Rahul' runs='12000' match='100' century='20' fifties='20' photo='http://picsum.photos/300?random=1' />    
+             <Player name='Pant' runs='15000' match='111' century='15' fifties='11' photo='http://picsum.photos/300?random=2' />    
+            
+            </div>
+          </div>
+          );
+    }
+}
+root.render(<Cricket />);
