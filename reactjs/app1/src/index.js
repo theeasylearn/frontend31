4142e50266681b005ba7469ee45438b73cf06ce2
theeasylearn <theeasylearn@gmail.com>
2024-09-27 13:42:24 +0530

    delete and like and dislike

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,36 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-class UserPost extends React.Component
-{
-    constructor(props)
-    {
+class UserPost extends React.Component {
+    constructor(props) {
         super(props);
         //create propery variable
         this.username = props.username;
         this.photo = props.photo;
         this.content = props.content;
-
         //create state array
         this.state = {
             comments: [],
             message: '',
+            isLiked:0
         }
     }
 
     onChangeValue = (e) => {
         this.setState({
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
     addComment = () => {
         console.log(this.state.message);
-        this.state.comments.unshift(this.state.message);
-         this.setState({
-            message:''
-         });
+        if (this.state.message.length > 0) {
+            this.state.comments.unshift(this.state.message);
+            this.setState({
+                message: ''
+            });
+        }
     }
-    render()
-    {
+    deleteComment = (item) => {
+        let temp = this.state.comments.filter((msg) => {
+           if(msg !== item)
+                return msg;
+        });
+        this.setState({
+            comments:temp
+        });
+    }
+
+    updateLike = () => {
+        this.setState({
+            isLiked: !this.state.isLiked
+        });
+    }
+    render() {
         return (<div className="col-4">
             <div className="card shadow">
                 {/* Post Header (User Info) */}
@@ -44,20 +58,22 @@ class UserPost extends React.Component
                 {/* Post Body (Text and Image) */}
                 <div className="card-body">
                     <p className="card-text">
-                      {this.content}
+                        {this.content}
                     </p>
-                    <img src={this.photo} className="img-fluid rounded" alt="Post Image" /> <br/>
+                    <img src={this.photo} className="img-fluid rounded" alt="Post Image" /> <br />
                     <b>Comments</b>
                     <ul className="list-group list-group-flush">
-                      {this.state.comments.map((item) => {
-                          return (<li className='list-group-item'>{item}</li>)  
-                      })}
+                        {this.state.comments.map((item) => {
+                            return (<li className='list-group-item d-flex justify-content-between'><span>{item}</span>
+                                <i onClick={() => this.deleteComment(item)} className='fa fa-trash'></i>
+                            </li>)
+                        })}
                     </ul>
                 </div>
                 {/* Post Footer (Like, Comment, Share Buttons) */}
                 <div className="card-footer d-flex justify-content-between">
-                    <button className="btn btn-light btn-sm">
-                        <i className="bi bi-hand-thumbs-up" /> Like
+                    <button className="btn btn-light btn-sm" onClick={this.updateLike}>
+                        <i className={(this.state.isLiked==true) ? "fa-solid fa-thumbs-up fa-2x" : "fa-regular fa-thumbs-up fa-2x"}></i>
                     </button>
                     <input type='text' className='form-control' name='message' onChange={(e) => this.onChangeValue(e)} value={this.state.message} />
                     <button className="btn btn-light btn-sm" onClick={this.addComment}>
@@ -76,8 +92,7 @@ class FaceBook extends React.Component {
 
     }
 
-    render()
-    {
+    render() {
         return (<div className="container my-4">
             <div className="row">
                 <UserPost username='Ankit Patel' photo='http://picsum.photos/300?random=1' content='this my first facebook post' />
