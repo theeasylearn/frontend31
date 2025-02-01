156c7e78b7fc2a44ea3d83d3341e77ed233f773b
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2025-02-01 11:31:51 +0530

    Add files via upload

---
+++ nodejs/mysql2/bill.js
@@ -0,0 +1,90 @@
+var connection = require("./connection");
+var common = require('./common');
+module.exports.insert = function (request, response) {
+    let requiredFields = ['usersid','fullname','address1','address2','city','pincode','paymentmode','remarks','mobile'];
+
+    let missingFields = common.getMissingFields(request.body,requiredFields);
+    if(missingFields.length>=1)
+    {
+        response.json([{'error':'following inputs are missing ' + missingFields.join(",")}])
+    }
+    else 
+    {
+        let {usersid,fullname,address1,address2,city,pincode,paymentmode,remarks,mobile} = request.body;
+        let sql = "select p.id,title,p.price,stock,quantity from product p, cart c where billid=0  and usersid=? and c.productid=p.id ";
+        let data = [usersid];
+        connection.db.query(sql,data,function(error,result){
+            if (error) {
+                response.json([{ 'error': 'error occured' }]);
+                console.log(error);
+            }
+            else {
+                //check is there any unpaid product in cart (where billid =0)
+                if(result.length === 0)
+                {
+                    response.json([{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'cart is empty' }]);
+                }
+                else 
+                {
+                    //1) check all the items in order are in stock or not 
+                    let orderdetail = [...result];
+                    let outofstock = [];
+
+                    outofstock = orderdetail.filter((item) => {
+                        if (item.stock < item.quantity)
+                            return item;
+                    });
+
+                    if (outofstock.length >= 1) {
+                        console.log(outofstock);
+                        let titles = outofstock.map(item => item.title).join(', ');
+                        response.json([{ 'error': 'no' }, { 'success': 'no' }, { 'message': 'following items are not in stock \n' + titles }]);
+                    }
+                    else {
+                        //calculate total bill amount 
+                        var grandtotal = 0;
+                        orderdetail.map((item) => {
+                            grandtotal += (item.price * item.quantity);
+                        });
+                        //reduce the stock of items given in order 
+                        orderdetail.map((item) => {
+                            sql = `update product set stock=stock-${item.quantity} where id=${item.id}`;
+                            connection.db.query(sql, function (error, result2) {
+                                if (error) {
+                                    response.json([{ 'error': 'error occured' }]);
+                                    console.log(error);
+                                }
+                            })
+                        });
+                        // insert new record into bill table 
+                        sql = "INSERT INTO `bill`(`usersid`, `fullname`, `address1`, `address2`, `mobile`, `city`, `pincode`,`amount`, `remarks`) VALUES (?,?,?,?,?,?,?,?,?)";
+                        let data2 = [usersid, fullname, address1, address2, mobile, city, pincode, grandtotal, remarks];
+                        connection.db.query(sql, data2, function (error, result2) {
+                            if (error) {
+                                response.json([{ 'error': 'error occured' }]);
+                                console.log(error);
+                            }
+                            else {
+                                //update cart 
+                                orderdetail.map((item) => {
+                                    var billid = result2.insertId;
+                                    console.log(billid);
+                                    sql = `update cart set billid=${billid},price=${item.price} where billid=0 and productid=${item.id} and usersid=${usersid}`;
+                                    console.log(sql);
+                                    connection.db.query(sql, function (error, result3) {
+                                        if (error) {
+                                            response.json([{ 'error': 'error occured' }]);
+                                            console.log(error);
+                                        }
+                                    });
+                                });
+                                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'new order created with order id ' + result2.insertId }]);
+                            }
+                        })
+
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ nodejs/mysql2/cart.js
@@ -0,0 +1,51 @@
+var connection = require("./connection");
+var common = require('./common');
+module.exports.select = function (request, response) {
+    let sql = `select c.id,quantity,title,photo,p.price from cart c,product p where usersid=? and billid=0 and c.productid=p.id`;
+    let usersid = request.param.userid;
+    let data = [usersid];
+    connection.db.query(sql, function (error, result) {
+        if (error != null)
+            response.json([{ 'error': "error occured" }]);
+        else {
+            result.unshift({ 'total': result.length });
+            result.unshift({ 'error': 'no' });
+            response.send(result);
+        }
+    });
+}
+module.exports.insert = function (request, response) {
+    let {productid,userid} = request.body;
+    let sql = `INSERT INTO cart (productid, quantity, price, usersid, billid)
+    VALUES (?, 1, 0, ?, 0)
+    ON DUPLICATE KEY UPDATE 
+    quantity = quantity + 1` 
+    let data = [productid,userid];
+    connection.db.query(sql,data,function(error,result){
+        if(error)
+        {
+            response.json([{ 'error': 'error occured' }]);
+            console.log(error);
+        }
+        else 
+            response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'cart updated' }]);
+    });
+}
+
+module.exports.delete = function (request, response) {
+    let sql = `delete from cart where id=?`;
+    let id = request.body.id;
+    let data = [id];
+    connection.db.query(sql,data,function(error,result){
+        if(error)
+        {
+            response.json([{ 'error': 'error occured' }]);
+            console.log(error);
+        }
+        else 
+        {
+            response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'cart deleted' }]);
+        }
+    });
+
+}
